f1addb61b24930424e9a4e129652414b55ab4f4c
Nestor Eduardo De la Cruz <nestoredelacruz@gmail.com>
2016-07-26 19:06:55 -0300

    Some restrictions are added to the model Book (#11)
    * Some restrictions added to the Book model
    * Added unit tests that prove validation works as expected

--- BookClub.Tests/Models/BookTest.cs
+++ BookClub.Tests/Models/BookTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -25,7 +26,85 @@ namespace DevDo.BookClub.Tests.Models
                     ISBN = "020161622X",
                     CoverUrl = "https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX258_BO1,204,203,200_.jpg"
                 };
+                var ValidationResult = new List<ValidationResult>();
+                var actual = Validator.TryValidateObject(book, new ValidationContext(book), ValidationResult, true);
+                Assert.True(actual, "Expected validation to pass.");
+            }
+
+            [Fact]
+            public void BookHasAnISBNWithMoreThanThirteenDigits_ReturnsValidationError()
+            {
+                var book = new DevDo.BookClub.Models.Book
+                {
+                    Title = "The Pragmatic Programmer",
+                    Author = "Andy Hunt",
+                    ISBN = "020161622X9409",
+                    CoverUrl = "https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX258_BO1,204,203,200_.jpg"
+                };
+                var ValidationResult = new List<ValidationResult>();
+                var actual = Validator.TryValidateObject(book, new ValidationContext(book), ValidationResult, true);
+                Assert.False(actual, "Expected validation to fail.");
+            }
+
+            [Fact]
+            public void BookDoesntHaveTitle_ReturnsValidationError()
+            {
+                var book = new DevDo.BookClub.Models.Book
+                {
+                    Title = null,
+                    Author = "Andy Hunt",
+                    ISBN = "020161622X",
+                    CoverUrl = "https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX258_BO1,204,203,200_.jpg"
+                };
+                var ValidationResult = new List<ValidationResult>();
+                var actual = Validator.TryValidateObject(book, new ValidationContext(book), ValidationResult, true);
+                Assert.False(actual, "Expected validation to fail.");
+            }
+
+            [Fact]
+            public void BookDoesntHaveAuthor_ReturnsValidationError()
+            {
+                var book = new DevDo.BookClub.Models.Book
+                {
+                    Title = "The Pragmatic Programmer",
+                    Author = null,
+                    ISBN = "020161622X",
+                    CoverUrl = "https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX258_BO1,204,203,200_.jpg"
+                };
+                var ValidationResult = new List<ValidationResult>();
+                var actual = Validator.TryValidateObject(book, new ValidationContext(book), ValidationResult, true);
+                Assert.False(actual, "Expected validation to fail.");
+            }
+
+            [Fact]
+            public void BookDoesntHaveISBN_ReturnsValidationError()
+            {
+                var book = new DevDo.BookClub.Models.Book
+                {
+                    Title = "The Pragmatic Programmer",
+                    Author = "Andy Hunt",
+                    ISBN = null,
+                    CoverUrl = "https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX258_BO1,204,203,200_.jpg"
+                };
+                var ValidationResult = new List<ValidationResult>();
+                var actual = Validator.TryValidateObject(book, new ValidationContext(book), ValidationResult, true);
+                Assert.False(actual, "Expected validation to fail.");
+            }
+
+            [Fact]
+            public void BookDoesntHaveCover_ReturnsValidationError()
+            {
+                var book = new DevDo.BookClub.Models.Book
+                {
+                    Title = "The Pragmatic Programmer",
+                    Author = "Andy Hunt",
+                    ISBN = "020161622X",
+                    CoverUrl = null
+                };
+                var ValidationResult = new List<ValidationResult>();
+                var actual = Validator.TryValidateObject(book, new ValidationContext(book), ValidationResult, true);
+                Assert.False(actual, "Expected validation to fail.");
             }
         }
+        }
     }
-}
--- BookClub/Models/Book.cs
+++ BookClub/Models/Book.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DevDo.BookClub.Models
 {
     public class Book
     {
-       
+        [Required(ErrorMessage = "El campo título es requerido.")]
+        [Display(Name = "Titulo")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "El campo autor es requerido.")]
+        [Display(Name = "Autor")]
         public string Author { get; set; }
+
+        [StringLength(13)]
+        [Required(ErrorMessage = "El campo ISBN es requerido.")]
         public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "La imágen es requerida..")]
+        [Display(Name = "Imágen")]
         public string CoverUrl { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ISBN.Length > 13)
+            {
+                yield return new ValidationResult("El ISBN no puede contener mas de 13 digitos");
+            }
+        }
     }
 }
